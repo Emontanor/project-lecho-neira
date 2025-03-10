32681b9485b759d851420838115c8329fa9a13f0
Fede <fhernandezm@unal.edu.co>
2025-03-10 18:38:41 -0500

    conexion generalizada

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import * as dotenv from 'dotenv';
 
+dotenv.config();
+
+const mongoUri = process.env.MONGO_URI;
+if (!mongoUri) {
+  throw new Error('❌ ERROR: La variable de entorno MONGO_URI no está definida.');
+}
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true,envFilePath:'.env',}),MongooseModule.forRoot(mongoUri),],
   controllers: [AppController],
   providers: [AppService],
 })
